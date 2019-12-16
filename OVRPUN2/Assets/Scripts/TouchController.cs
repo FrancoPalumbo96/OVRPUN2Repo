64b89aed2c1baa1d8a16d1f7c85b6a96a9ec869f
Franco Palumbo <palumbofranco9a@gmail.com>
2019-12-16 14:10:16 -0300

    First commit

---
+++ OVRPUN2/Assets/Scripts/TouchController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour
+{
+
+    public PhotonLobby PhotonLobby;
+    private void OnCollisionEnter(Collision other)
+    {
+        if(!other.gameObject.name.Contains("CustomHand")) return;
+        GetComponent<Renderer>().material.color = Color.blue;
+        PhotonLobby.BattleCubeTouched();
+        
+    }
+
+    private void OnCollisionExit(Collision other)
+    {
+        if(!other.gameObject.name.Contains("CustomHand")) return;
+        GetComponent<Renderer>().material.color = Color.green;
+    }
+}
