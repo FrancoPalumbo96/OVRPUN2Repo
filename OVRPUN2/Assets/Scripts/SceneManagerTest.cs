46dbd7fe791d5b0c605c43b7ec8e7929dea2874e
Franco Palumbo <franco.palumbo@ing.austral.edu.ar>
2019-12-26 17:21:45 -0300

    Avatar tracking multplaayer

---
+++ OVRPUN2/Assets/Scripts/SceneManagerTest.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagerTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start() {
+        SceneManager.LoadScene("DobleOVR2", LoadSceneMode.Additive);
+    }
+}
