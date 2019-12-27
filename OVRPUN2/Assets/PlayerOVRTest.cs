6859310a0feab547626a25f0828ae37f9545176d
Franco Palumbo <palumbofranco9a@gmail.com>
2019-12-27 11:43:50 -0300

    Destroy ovr when is not mine

---
+++ OVRPUN2/Assets/PlayerOVRTest.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class PlayerOVRTest : MonoBehaviour
+{
+    private PhotonView PV;
+    void Start()
+    {
+        PV = GetComponent<PhotonView>();
+        if (!PV.IsMine)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
