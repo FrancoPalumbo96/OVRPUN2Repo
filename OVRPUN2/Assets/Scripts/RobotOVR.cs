b9c42905840099dac5fecac0d97a2f077b427e9a
Franco Palumbo <franco.palumbo@ing.austral.edu.ar>
2019-12-27 14:24:47 -0300

    robot waving hand

--- OVRPUN2/Assets/Scripts/RobotOVR.cs
+++ OVRPUN2/Assets/Scripts/RobotOVR.cs
@@ -4,15 +4,30 @@ using System.IO;
 using Photon.Pun;
 using UnityEngine;
 
-public class RobotOVR : MonoBehaviour
-{
+public class RobotOVR : MonoBehaviour {
+    
+    private static RobotOVR instance = null;
     public GameObject ovrPrefab;
+    private PhotonView PV;
+    private VRRig vrRig;
 
     private VRRig _rig;
     // Start is called before the first frame update
-    void Start()
-    {
-        if (GetComponentInParent<PhotonView>().IsMine) {
+    void Start() {
+
+        vrRig = GetComponent<VRRig>();
+        if (instance == null) {
+            instance = this;
+            Debug.Log("INSTANCE EQUAL TO THIS");
+        }
+        else if(instance != this) {
+            Debug.Log("DOBLE ROBOT DELETED");
+            Destroy(vrRig);
+            return;
+        }
+        
+        PV = GetComponent<PhotonView>();
+        if (PV.IsMine) {
 
             GameObject ovr = Instantiate(ovrPrefab, this.transform.position, ovrPrefab.transform.rotation);
             _rig = GetComponent<VRRig>();
@@ -29,6 +44,9 @@ public class RobotOVR : MonoBehaviour
                 .Find(ovrName + "/OVRPlayerController/OVRCameraRig/TrackingSpace/LeftHandAnchor").transform;
             _rig.rightHand.vrTarget = GameObject
                 .Find(ovrName + "/OVRPlayerController/OVRCameraRig/TrackingSpace/RightHandAnchor").transform;
+        }
+        else {
+            Destroy(vrRig);
         }
     }
 
