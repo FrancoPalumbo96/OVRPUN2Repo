681a0ee9c0ac45c0e1cc62f2ecdb41abe2037ee5
Franco Palumbo <franco.palumbo@ing.austral.edu.ar>
2020-01-02 16:11:10 -0300

    Added ovrgrabbablepandu

---
+++ OVRPUN2/Assets/MaterialPerOwner.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using Photon.Pun;
+
+
+[RequireComponent( typeof( PhotonView ) )]
+public class MaterialPerOwner : MonoBehaviourPun
+{
+    private string assignedColorForUserId;
+
+    Renderer m_Renderer;
+
+    void Start()
+    {
+        m_Renderer = GetComponent<Renderer>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        /*if( this.photonView.Owner.UserId != assignedColorForUserId )
+        {
+            int index = System.Array.IndexOf(ExitGames.UtilityScripts.PlayerRoomIndexing.instance.PlayerIds, this.photonView.ownerId);
+            try
+            {
+                m_Renderer.material.color = FindObjectOfType<ColorPerPlayer>().Colors[index];
+                this.assignedColorForUserId = this.photonView.Owner.UserId;
+            }
+            catch (Exception e)
+            {
+                //nothing
+            }
+           
+            //Debug.Log("Switched Material to: " + this.assignedColorForUserId + " " + this.renderer.material.GetInstanceID());
+        }*/
+    }
+}
---
+++ OVRPUN2/Assets/OnClickRequestOwnership.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Photon.Pun;
+using UnityEngine;
+
+public class OnClickRequestOwnership : MonoBehaviourPun {
+
+    public void OnClick() {
+        if (Input.GetKey(KeyCode.LeftShift) || Input.GetKey(KeyCode.RightShift)) {
+            Vector3 colVector =
+                new Vector3(Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f), Random.Range(0.0f, 1.0f));
+            this.photonView.RPC("ColorRpc", RpcTarget.AllBufferedViaServer, colVector);
+        }
+        else {
+            if (photonView.Owner.UserId.CompareTo(PhotonNetwork.LocalPlayer.UserId) == 0) {
+                Debug.Log("Not requesting ownership. Already mine.");
+                return;
+            }
+
+            photonView.RequestOwnership();
+        }
+    }
+
+    [PunRPC]
+    public void ColorRpc(Vector3 col) {
+        Color color = new Color(col.x, col.y, col.z);
+        this.gameObject.GetComponent<Renderer>().material.color = color;
+    }
+}
