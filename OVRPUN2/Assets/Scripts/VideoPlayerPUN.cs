0c772f1e90d72d5875da4869e874473bab6afcc6
Franco Palumbo <franco.palumbo@ing.austral.edu.ar>
2020-01-06 16:20:05 -0300

    Correct video sync

--- OVRPUN2/Assets/Scripts/VideoPlayerPUN.cs
+++ OVRPUN2/Assets/Scripts/VideoPlayerPUN.cs
@@ -9,21 +9,27 @@ public class VideoPlayerPUN : MonoBehaviourPun, IPunObservable {
     // Start is called before the first frame update
     void Start() {
         videoPlayer = GetComponent<VideoPlayer>();
+        videoPlayer.loopPointReached += SyncVideo;
+    }
+    
+    
+    private void SyncVideo(VideoPlayer vp) {
+        photonView.RPC("setVideoTime", RpcTarget.AllBuffered);
     }
 
-    /*[PunRPC]*/
-    private void setVideoTime(double videoTime) {
-        videoPlayer.time = videoTime;
+    [PunRPC]
+    private void setVideoTime() {
+        videoPlayer.time = 0;
         Debug.Log("Video Time: " + videoPlayer.time);
     }
 
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
-        if (stream.IsWriting) {
+        /*if (stream.IsWriting) {
             stream.SendNext(videoPlayer.time);
         }
 
         if (stream.IsReading) {
             setVideoTime((double)stream.ReceiveNext());
-        }
+        }*/
     }
 }
