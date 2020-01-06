f20b33109d5b32b026b6c33f2fba8c1b09c13091
Franco Palumbo <franco.palumbo@ing.austral.edu.ar>
2020-01-06 16:02:13 -0300

    Video player sync test

---
+++ OVRPUN2/Assets/Scripts/VideoPlayerPUN.cs
@@ -0,0 +1,29 @@
+﻿
+using Photon.Pun;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoPlayerPUN : MonoBehaviourPun, IPunObservable {
+    private VideoPlayer videoPlayer;
+    
+    // Start is called before the first frame update
+    void Start() {
+        videoPlayer = GetComponent<VideoPlayer>();
+    }
+
+    /*[PunRPC]*/
+    private void setVideoTime(double videoTime) {
+        videoPlayer.time = videoTime;
+        Debug.Log("Video Time: " + videoPlayer.time);
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info) {
+        if (stream.IsWriting) {
+            stream.SendNext(videoPlayer.time);
+        }
+
+        if (stream.IsReading) {
+            setVideoTime((double)stream.ReceiveNext());
+        }
+    }
+}
