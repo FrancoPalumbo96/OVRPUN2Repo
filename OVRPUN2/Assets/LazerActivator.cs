7526fee04a32ea4b1266429f0cb19098f53fd809
= <=>
2020-01-14 12:46:02 -0300

    Test lazer, found problems with gosht player and robot player when in scene at same time

--- OVRPUN2/Assets/LazerActivator.cs
+++ OVRPUN2/Assets/LazerActivator.cs
@@ -1,43 +1,64 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Photon.Pun;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class LazerActivator : MonoBehaviour {
+public class LazerActivator : MonoBehaviourPun {
 
     public GameObject visualLazer;
     public GameObject lazerEventSystem;
 
     private bool isLazerActive = false;
+
+    private PhotonView photonView;
     
     
     // Start is called before the first frame update
     void Start()
     {
-        DeactivateLazer();
+//        photonView = transform.parent.parent.GetComponent<PhotonView>();
+        photonView = GetComponent<PhotonView>();
+
+        if(!photonView.IsMine) return;
+
+        photonView.RPC("deactivateLazer", RpcTarget.AllBuffered); 
+
     }
 
     private void Update() {
         LazerManager();
+        //photonView.RPC("SendRPC", RpcTarget.MasterClient);   
     }
 
+ 
     private void LazerManager() {
-        if (OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.RTouch)) {
-            
-            if (isLazerActive) {
-                DeactivateLazer();
-            }
-            else {
-                ActivateLazer();
-            }
-            
+        if(!photonView.IsMine) return;
+        if (!OVRInput.GetDown(OVRInput.Button.One, OVRInput.Controller.RTouch)) return;
+        if (isLazerActive) {
+            visualLazer.SetActive(false);
+            lazerEventSystem.SetActive(false);
+            isLazerActive = false;
+            Debug.LogWarning("Lazer Deactivated");
+
+            photonView.RPC("deactivateLazer", RpcTarget.OthersBuffered); 
+//                DeactivateLazer();
+        }
+        else {
+            visualLazer.SetActive(true);
+            lazerEventSystem.SetActive(true);
+            isLazerActive = true;
+            Debug.LogWarning("Lazer Ativated");
+            photonView.RPC("activateLazer", RpcTarget.OthersBuffered); 
+//                ActivateLazer();
         }
         /*if (OVRInput.Get(OVRInput.Button.One, OVRInput.Controller.LTouch)) {
             Debug.LogWarning("Activated Lazer Left");
         }*/
     }
 
-    private void DeactivateLazer() {
+    [PunRPC]
+    private void deactivateLazer() {
         visualLazer.SetActive(false);
         lazerEventSystem.SetActive(false);
         isLazerActive = false;
@@ -45,11 +66,15 @@ public class LazerActivator : MonoBehaviour {
 
     }
 
-    private void ActivateLazer() {
+    [PunRPC]
+    private void activateLazer() {
         visualLazer.SetActive(true);
         lazerEventSystem.SetActive(true);
         isLazerActive = true;
         Debug.LogWarning("Lazer Ativated");
 
     }
+
+    
+    
 }
